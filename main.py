507c5fd840a7cb95368ad265cea022d9f5a11ba0
GOVIND P LAL <govindplal@gmail.com>
2026-06-20 21:24:53 +0530

    feat:ai streaming

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import asyncio
 
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from openai import AsyncOpenAI
 
 from models.chat import ChatRequest
+from core.config import settings
 
 app = FastAPI()
 
@@ -17,10 +19,23 @@ async def health():
 
 @app.post("/chat")
 async def stream_chat(payload: ChatRequest):
-    async def mock_generator():
-        chunks = ["Hello", " world", "!"]
-        for chunk in chunks:
-            yield chunk
-            await asyncio.sleep(0.5)
 
-    return StreamingResponse(mock_generator(), media_type="text/event-stream")
+    client = AsyncOpenAI(
+        base_url= settings.OPENAI_API_BASE_URL,
+        api_key= settings.OPENAI_API_KEY,
+        default_headers={"ngrok-skip-browser-warning": "true"}
+    )
+
+    async def openai_generator():
+
+        stream = await client.chat.completions.create(
+            model=settings.LLM_MODEL_NAME,
+            messages=[{"role": m.role, "content": m.content} for m in payload.messages],
+            stream=True,
+        )
+        async for chunk in stream:  
+            if chunk.choices and chunk.choices[0].delta.content is not None:
+                text_chunk = chunk.choices[0].delta.content
+                yield text_chunk
+
+    return StreamingResponse(openai_generator(), media_type="text/event-stream")
